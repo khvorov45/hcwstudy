5aa43a8b2bb47b1d31b881d7dc22da998ef60ed1
Arseniy Khvorov <khvorov45@gmail.com>
2021-09-15 09:13:23 +1000

    add counts for 2020 recruitments

--- data-summary/counts.R
+++ data-summary/counts.R
@@ -17,40 +17,59 @@ at_least_one_bleed_in_2021 <- bleed_dates %>%
 participants_currently_enrolled <- participants_with_age_at_screening_cat %>%
   filter(pid %in% at_least_one_bleed_in_2021)
 
-participants_currently_enrolled_counts_long <- participants_currently_enrolled %>%
-  group_by(site, age_at_screening_cat) %>%
-  summarise(.groups = "drop", count = n(), gender = "overall") %>%
-  arrange(age_at_screening_cat) %>%
-  bind_rows(
-    participants_currently_enrolled %>%
-      group_by(age_at_screening_cat) %>%
-      summarise(.groups = "drop", count = n(), gender = "overall", site = "overall")
-  ) %>%
-  bind_rows(
-    participants_currently_enrolled %>%
-      group_by(gender) %>%
-      summarise(.groups = "drop", count = n(), age_at_screening_cat = "overall", site = "overall")
-  ) %>%
-  bind_rows(
-    participants_currently_enrolled %>%
-      group_by(site, gender) %>%
-      summarise(.groups = "drop", count = n(), age_at_screening_cat = "overall")
-  ) %>%
-  bind_rows(
-    participants_currently_enrolled %>%
-      group_by(site) %>%
-      summarise(count = n(), gender = "overall", age_at_screening_cat = "overall")
-  ) %>%
-  bind_rows(
-    participants_currently_enrolled %>%
-      summarise(count = n(), site = "overall", gender = "overall", age_at_screening_cat = "overall")
+calc_counts <- function(data) {
+  data %>%
+    group_by(site, age_at_screening_cat) %>%
+    summarise(.groups = "drop", count = n(), gender = "overall") %>%
+    arrange(age_at_screening_cat) %>%
+    bind_rows(
+      data %>%
+        group_by(age_at_screening_cat) %>%
+        summarise(.groups = "drop", count = n(), gender = "overall", site = "overall")
+    ) %>%
+    bind_rows(
+      data %>%
+        group_by(gender) %>%
+        summarise(.groups = "drop", count = n(), age_at_screening_cat = "overall", site = "overall")
+    ) %>%
+    bind_rows(
+      data %>%
+        group_by(site, gender) %>%
+        summarise(.groups = "drop", count = n(), age_at_screening_cat = "overall")
+    ) %>%
+    bind_rows(
+      data %>%
+        group_by(site) %>%
+        summarise(count = n(), gender = "overall", age_at_screening_cat = "overall")
+    ) %>%
+    bind_rows(
+      data %>%
+        summarise(count = n(), site = "overall", gender = "overall", age_at_screening_cat = "overall")
+    )
+}
+
+participants_currently_enrolled_counts_long <-
+  calc_counts(participants_currently_enrolled)
+
+participants_currently_enrolled_recruited_in_2020_counts_long <-
+  calc_counts(
+    participants_currently_enrolled %>% filter(recruitment_year == 2020)
   )
 
 participants_currently_enrolled_counts_wide <-
   participants_currently_enrolled_counts_long %>%
   pivot_wider(names_from = "site", values_from = "count")
 
+participants_currently_enrolled_recruited_in_2020_counts_wide <-
+  participants_currently_enrolled_recruited_in_2020_counts_long %>%
+  pivot_wider(names_from = "site", values_from = "count")
+
 write_csv(
   participants_currently_enrolled_counts_wide,
   "data-summary/counts-currently-enrolled.csv"
+)
+
+write_csv(
+  participants_currently_enrolled_recruited_in_2020_counts_wide,
+  "data-summary/counts-currently-enrolled-recruited-in-2020.csv"
 )
