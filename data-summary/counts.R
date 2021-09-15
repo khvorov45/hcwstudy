2a60677e11bf421eb87960655f9ddab1c84fdca0
Arseniy Khvorov <khvorov45@gmail.com>
2021-09-15 11:18:08 +1000

    add prior vaccination counts

--- data-summary/counts.R
+++ data-summary/counts.R
@@ -7,6 +7,10 @@ participants_with_age_at_screening_cat <- participants %>%
 
 bleed_dates <- read_csv("data/bleed-dates.csv", col_types = cols())
 
+vaccinations <- read_csv("data/vaccinations.csv", col_types = cols())
+
+# SECTION Currently enrolled
+
 at_least_one_bleed_in_2021 <- bleed_dates %>%
   filter(year == 2021) %>%
   group_by(pid) %>%
@@ -48,20 +52,22 @@ calc_counts <- function(data) {
     )
 }
 
+participants_counts_long <- calc_counts(participants_with_age_at_screening_cat)
+
+participants_counts_wide <-
+  participants_counts_long %>%
+  pivot_wider(names_from = "site", values_from = "count")
+
+write_csv(
+  participants_counts_wide,
+  "data-summary/counts-all.csv"
+)
+
 participants_currently_enrolled_counts_long <-
   calc_counts(participants_currently_enrolled)
 
-participants_currently_enrolled_recruited_in_2020_counts_long <-
-  calc_counts(
-    participants_currently_enrolled %>% filter(recruitment_year == 2020)
-  )
-
 participants_currently_enrolled_counts_wide <-
   participants_currently_enrolled_counts_long %>%
-  pivot_wider(names_from = "site", values_from = "count")
-
-participants_currently_enrolled_recruited_in_2020_counts_wide <-
-  participants_currently_enrolled_recruited_in_2020_counts_long %>%
   pivot_wider(names_from = "site", values_from = "count")
 
 write_csv(
@@ -69,7 +75,40 @@ write_csv(
   "data-summary/counts-currently-enrolled.csv"
 )
 
+participants_currently_enrolled_recruited_in_2020_counts_long <-
+  calc_counts(
+    participants_currently_enrolled %>% filter(recruitment_year == 2020)
+  )
+
+participants_currently_enrolled_recruited_in_2020_counts_wide <-
+  participants_currently_enrolled_recruited_in_2020_counts_long %>%
+  pivot_wider(names_from = "site", values_from = "count")
+
 write_csv(
   participants_currently_enrolled_recruited_in_2020_counts_wide,
   "data-summary/counts-currently-enrolled-recruited-in-2020.csv"
 )
+
+# SECTION Vaccination history for the nested study
+
+vaccinations_with_participant_info <-
+  inner_join(vaccinations, participants %>% select(pid, arm, recruitment_year, site), "pid")
+
+prior_vaccinations <- vaccinations_with_participant_info %>%
+  filter(year >= recruitment_year - 5, year < recruitment_year) %>%
+  group_by(pid, arm, site) %>%
+  summarise(.groups = "drop", n_prior_vaccinations = sum(status %in% c("Australia", "Overseas")))
+
+prior_vaccinations_nested_only <- prior_vaccinations %>% filter(arm == "nested")
+
+prior_vaccinations_nested_only_counts <- prior_vaccinations_nested_only %>%
+  group_by(n_prior_vaccinations, site) %>%
+  summarise(.groups = "drop", count = n()) %>%
+  bind_rows(
+    prior_vaccinations_nested_only %>%
+      group_by(n_prior_vaccinations) %>%
+      summarise(count = n(), site = "overall")
+  ) %>%
+  pivot_wider(names_from = "site", values_from = "count")
+
+write_csv(prior_vaccinations_nested_only_counts, "data-summary/prior-vaccinations-nested-only.csv")
