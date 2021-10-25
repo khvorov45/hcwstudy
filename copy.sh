7f2bfc74c9bcde76d9162b4f7f3b84d2188217c8
Arseniy Khvorov <khvorov45@gmail.com>
2021-10-26 09:15:47 +1100

    add a target location

--- copy.sh
+++ copy.sh
@@ -1,2 +1,5 @@
 rsync -rv /home/khvorova/Projects/hcwstudy/ \
     /home/khvorova/vidrlwhoflu/Group/personnel/Arseniy/hcwstudy/
+
+rsync -rv /home/khvorova/Projects/hcwstudy/ \
+    "/home/khvorova/vidrlwhoflu/Research/2019.176 NIH HCW Study/arseniy"
