058bc182d27e1f1b5dbd6ce4d4e3ad2dd47e8196
Arseniy Khvorov <khvorov45@gmail.com>
2022-04-06 12:06:11 +1000

    add prior vacs

--- reports-app/backend/prepare-data.R
+++ reports-app/backend/prepare-data.R
@@ -1,7 +1,18 @@
 library(tidyverse)
 
+vac_hist <- read_csv("../data/vaccinations.csv")
+
+prior_vac_counts <- vac_hist %>% 
+	group_by(pid) %>%
+	summarise(
+		prior2020 = sum(year >= 2015 & year < 2020 & (status == "Australia" | status == "Overseas")),
+		prior2021 = sum(year >= 2016 & year < 2021 & (status == "Australia" | status == "Overseas")),
+		prior2022 = sum(year >= 2017 & year < 2022 & (status == "Australia" | status == "Overseas")),
+	)
+
 participants <- read_csv("../data/participants.csv") %>%
-	mutate(age_group = cut(age_screening, c(-Inf, 18, 30, 50, 65), right = FALSE))
+	mutate(age_group = cut(age_screening, c(-Inf, 18, 30, 50, 65), right = FALSE)) %>%
+	left_join(prior_vac_counts, "pid")
 
 withdrawn <- read_csv("../data/withdrawn.csv") %>%
 	left_join(participants %>% select(pid, site), "pid") %>%
