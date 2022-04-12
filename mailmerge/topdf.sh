6f2940c80fd19c92eea5f218b00abf2a00a75976
Arseniy Khvorov <khvorov45@gmail.com>
2022-04-12 13:23:33 +1000

    topdf script

---
+++ mailmerge/topdf.sh
@@ -0,0 +1,13 @@
+cd mailmerge/ALF
+#lowriter --convert-to pdf *.docx
+cd ../CHW
+#lowriter --convert-to pdf *.docx
+cd ../JHH
+#lowriter --convert-to pdf *.docx
+cd ../PCH
+lowriter --convert-to pdf *.docx
+cd ../QCH
+#lowriter --convert-to pdf *.docx
+cd ../WCH
+#lowriter --convert-to pdf *.docx
+cd ../..
