e45f2a4e333e2e845c987a33353079893bdbad99
Arseniy Khvorov <khvorov45@gmail.com>
2022-05-25 10:43:04 +1000

    migrate another

--- migration/migration.R
+++ migration/migration.R
@@ -179,6 +179,9 @@ redcap_upload(
 jhh_806_2021_record_id <- pid2021 %>% filter(pid == "JHH-806") %>% pull(record_id)
 jhh_806_2022_record_id <- pid2022_current %>% filter(pid == "JHH-806") %>% pull(record_id)
 
+pch_043_2021_record_id <- pid2021 %>% filter(pid == "PCH-043") %>% pull(record_id)
+pch_043_2022_record_id <- pid2022_current %>% filter(pid == "PCH-043") %>% pull(record_id)
+
 #
 # SECTION Screening
 #
@@ -287,6 +290,15 @@ redcap_upload(
     filter(record_id == jhh_806_2021_record_id) %>%
     process_baseline() %>%
     mutate(record_id = jhh_806_2022_record_id)
+)
+
+redcap_upload(
+  2022,
+  "baseline_arm_1",
+  baseline2021 %>%
+    filter(record_id == pch_043_2021_record_id) %>%
+    process_baseline() %>%
+    mutate(record_id = pch_043_2022_record_id)
 )
 
 #
