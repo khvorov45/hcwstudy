09b632e1fc34243bb74e9ca2340ba2deb01256da
Arseniy Khvorov <khvorov45@gmail.com>
2022-06-21 14:17:09 +1000

    delete script

--- deploy-reports-app.sh
+++
@@ -1,3 +0,0 @@
-rsync -avz -e 'ssh' ./data unicloud:/home/ubuntu/hcwstudy
-rsync -avz -e 'ssh' ./data-raw unicloud:/home/ubuntu/hcwstudy
-rsync -avz -e 'ssh' ./reports-app unicloud:/home/ubuntu/hcwstudy
