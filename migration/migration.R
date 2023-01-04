fc3cae309305deb5f86d4d3500aaf38679545b90
Arseniy Khvorov <khvorov45@gmail.com>
2023-01-05 09:53:31 +1100

    update migration

--- migration/migration.R
+++ migration/migration.R
@@ -86,6 +86,7 @@ redcap_consent_request <- function(project_year) {
 
 consent2020 <- redcap_consent_request(2020)
 consent2021 <- redcap_consent_request(2021)
+consent2022 <- redcap_consent_request(2022)
 
 active2020 <- consent2020 %>%
   select(-contains("redcap")) %>%
@@ -97,23 +98,32 @@ active2021 <- consent2021 %>%
   filter(consent == 1 | !is.na(study_group_vacc) | consent_unvacc == 1) %>%
   filter(!record_id %in% withdrawn2021$record_id)
 
-redcap_upload_spoonfed(20, 2022, "baseline_arm_1", active2021)
+active2022 <- consent2022 %>%
+  select(-contains("redcap")) %>%
+  filter(consent == 1 | !is.na(study_group_vacc) | consent_unvacc == 1) %>%
+  filter(!record_id %in% withdrawn2022$record_id)
+
+# redcap_upload_spoonfed(20, 2023, "baseline_arm_1", active2023)
 
 data_access_groups2021 <- consent2021 %>%
   select(record_id, redcap_data_access_group) %>%
   filter(record_id %in% active2021$record_id)
 
+data_access_groups2022 <- consent2022 %>%
+  select(record_id, redcap_data_access_group) %>%
+  filter(record_id %in% active2022$record_id)
+
 # NOTE(sen) If data access group doesn't get assigned, the record will be invisible
 # unless you have full access.
 
-redcap_upload_spoonfed(20, 2022, "", data_access_groups2021)
+# redcap_upload_spoonfed(20, 2023, "", data_access_groups2022)
 
-redcap_upload(
-  "2022", "baseline_arm_1",
-  consent2021 %>%
-    filter(record_id == "168-4") %>%
-    select(record_id, redcap_data_access_group)
-)
+# redcap_upload(
+#   "2022", "baseline_arm_1",
+#   consent2021 %>%
+#     filter(record_id == "168-4") %>%
+#     select(record_id, redcap_data_access_group)
+# )
 
 #
 # SECTION PID, study group
@@ -123,71 +133,70 @@ redcap_pid_request <- function(project_year) {
   redcap_request(
     project_year,
     "baseline_arm_1",
-    "record_id,pid,recruit_year,studygroup_y1,main_vacc,main_unvacc,nested_naive,nested_infect",
+    "record_id,pid,recruit_year,studygroup_y1,studygroup_y2,main_vacc,main_unvacc,nested_naive,nested_infect",
     exportDataAccessGroups = "true",
     rawOrLabel = "raw"
   )
 }
 
 pid2021 <- redcap_pid_request(2021)
-pid2022_current <- redcap_pid_request(2022)
+pid2022 <- redcap_pid_request(2022)
 
 process_pid <- function(data) {
   data %>%
     mutate(
-      studygroup_y2 = case_when(
+      studygroup_y3 = case_when(
         nested_naive == 1 | nested_infect == 1 ~ 3,
         main_vacc == 1 ~ 1,
         main_unvacc == 1 ~ 2,
       ),
       recruit_year = replace_na(recruit_year, 2021)
     ) %>%
-    select(record_id, pid, recruit_year, studygroup_y1, studygroup_y2)
+    select(record_id, pid, recruit_year, studygroup_y1, studygroup_y2, studygroup_y3)
 }
 
-pid2022 <- pid2021 %>%
-  filter(record_id %in% active2021$record_id) %>%
+pid2022_active <- pid2022 %>%
+  filter(record_id %in% active2022$record_id) %>%
   process_pid()
 
 # NOTE(sen) Should be empty
-pid2022 %>% filter(is.na(studygroup_y2))
-pid2022 %>% filter(is.na(recruit_year))
+pid2022_active %>% filter(is.na(studygroup_y3))
+pid2022_active %>% filter(is.na(recruit_year))
 
-redcap_upload_spoonfed(20, 2022, "baseline_arm_1", pid2022)
+# redcap_upload_spoonfed(20, 2023, "baseline_arm_1", pid2022_active)
 
-redcap_upload(
-  2022, "baseline_arm_1", 
-  pid2021 %>% 
-    filter(pid == "JHH-060") %>% 
-    process_pid()
-)
+# redcap_upload(
+#   2022, "baseline_arm_1", 
+#   pid2021 %>% 
+#     filter(pid == "JHH-060") %>% 
+#     process_pid()
+# )
 
-redcap_upload(
-  2022, "baseline_arm_1", 
-  pid2021 %>% 
-    filter(pid == "JHH-021") %>% 
-    process_pid()
-)
+# redcap_upload(
+#   2022, "baseline_arm_1", 
+#   pid2021 %>% 
+#     filter(pid == "JHH-021") %>% 
+#     process_pid()
+# )
 
-redcap_upload(
-  2022, "baseline_arm_1", 
-  pid2021 %>% 
-    filter(pid == "ALF-804") %>% 
-    process_pid()
-)
+# redcap_upload(
+#   2022, "baseline_arm_1", 
+#   pid2021 %>% 
+#     filter(pid == "ALF-804") %>% 
+#     process_pid()
+# )
 
+# jhh_806_2021_record_id <- pid2021 %>% filter(pid == "JHH-806") %>% pull(record_id)
+# jhh_806_2022_record_id <- pid2022_current %>% filter(pid == "JHH-806") %>% pull(record_id)
 
-jhh_806_2021_record_id <- pid2021 %>% filter(pid == "JHH-806") %>% pull(record_id)
-jhh_806_2022_record_id <- pid2022_current %>% filter(pid == "JHH-806") %>% pull(record_id)
+# jhh_007_2021_record_id <- pid2021 %>% filter(pid == "JHH-007") %>% pull(record_id)
+# jhh_007_2022_record_id <- pid2022_current %>% filter(pid == "JHH-007") %>% pull(record_id)
 
-jhh_007_2021_record_id <- pid2021 %>% filter(pid == "JHH-007") %>% pull(record_id)
-jhh_007_2022_record_id <- pid2022_current %>% filter(pid == "JHH-007") %>% pull(record_id)
+# pch_043_2021_record_id <- pid2021 %>% filter(pid == "PCH-043") %>% pull(record_id)
+# pch_043_2022_record_id <- pid2022_current %>% filter(pid == "PCH-043") %>% pull(record_id)
 
-pch_043_2021_record_id <- pid2021 %>% filter(pid == "PCH-043") %>% pull(record_id)
-pch_043_2022_record_id <- pid2022_current %>% filter(pid == "PCH-043") %>% pull(record_id)
-
-pch_096_2021_record_id <- pid2021 %>% filter(pid == "PCH-096") %>% pull(record_id)
-pch_096_2022_record_id <- pid2022_current %>% filter(pid == "PCH-096") %>% pull(record_id)
+# pch_096_2021_record_id <- pid2021 %>% filter(pid == "PCH-096") %>% pull(record_id)
+# pch_096_2022_record_id <- pid2022_current %>% filter(pid == "PCH-096") %>% pull(record_id)
 
 #
 # SECTION Screening
@@ -204,40 +213,41 @@ redcap_screening_request <- function(project_year) {
 }
 
 screening2021 <- redcap_screening_request(2021)
+screening2022 <- redcap_screening_request(2022)
 
-screening2022 <- screening2021 %>%
-  filter(record_id %in% active2021$record_id) %>%
+screening2022_active <- screening2022 %>%
+  filter(record_id %in% active2022$record_id) %>%
   filter(!is.na(date_screening) | !is.na(site_name)) %>%
   select(-contains("redcap"))
 
-redcap_upload_spoonfed(20, 2022, "baseline_arm_1", screening2022)
+# redcap_upload_spoonfed(20, 2023, "baseline_arm_1", screening2022_active)
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  screening2021 %>%
-    filter(record_id == "170-71") %>%
-    filter(!is.na(date_screening) | !is.na(site_name)) %>%
-    select(-contains("redcap"))
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   screening2021 %>%
+#     filter(record_id == "170-71") %>%
+#     filter(!is.na(date_screening) | !is.na(site_name)) %>%
+#     select(-contains("redcap"))
+# )
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  screening2021 %>%
-    filter(record_id == "170-31") %>%
-    filter(!is.na(date_screening) | !is.na(site_name)) %>%
-    select(-contains("redcap"))
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   screening2021 %>%
+#     filter(record_id == "170-31") %>%
+#     filter(!is.na(date_screening) | !is.na(site_name)) %>%
+#     select(-contains("redcap"))
+# )
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  screening2021 %>%
-    filter(record_id == "168-4") %>%
-    filter(!is.na(date_screening) | !is.na(site_name)) %>%
-    select(-contains("redcap"))
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   screening2021 %>%
+#     filter(record_id == "168-4") %>%
+#     filter(!is.na(date_screening) | !is.na(site_name)) %>%
+#     select(-contains("redcap"))
+# )
 
 #
 # SECTION Baseline questionnaire
@@ -254,68 +264,69 @@ redcap_baseline_request <- function(project_year) {
 }
 
 baseline2021 <- redcap_baseline_request(2021)
+baseline2022 <- redcap_baseline_request(2022)
 
 process_baseline <- function(data) {
   data %>%
     select(-contains("redcap"))
 }
 
-baseline2022 <- baseline2021 %>%
-  filter(record_id %in% active2021$record_id, !is.na(baseline_q_date)) %>%
+baseline2022_active <- baseline2022 %>%
+  filter(record_id %in% active2022$record_id, !is.na(baseline_q_date)) %>%
   process_baseline()
 
-redcap_upload_spoonfed(10, 2022, "baseline_arm_1", baseline2022)
+# redcap_upload_spoonfed(10, 2023, "baseline_arm_1", baseline2022_active)
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  baseline2021 %>%
-    filter(record_id == "170-71") %>%
-    process_baseline()
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   baseline2021 %>%
+#     filter(record_id == "170-71") %>%
+#     process_baseline()
+# )
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  baseline2021 %>%
-    filter(record_id == "170-31") %>%
-    process_baseline()
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   baseline2021 %>%
+#     filter(record_id == "170-31") %>%
+#     process_baseline()
+# )
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  baseline2021 %>%
-    filter(record_id == "168-4") %>%
-    process_baseline()
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   baseline2021 %>%
+#     filter(record_id == "168-4") %>%
+#     process_baseline()
+# )
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  baseline2021 %>%
-    filter(record_id == jhh_806_2021_record_id) %>%
-    process_baseline() %>%
-    mutate(record_id = jhh_806_2022_record_id)
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   baseline2021 %>%
+#     filter(record_id == jhh_806_2021_record_id) %>%
+#     process_baseline() %>%
+#     mutate(record_id = jhh_806_2022_record_id)
+# )
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  baseline2021 %>%
-    filter(record_id == jhh_007_2021_record_id) %>%
-    process_baseline() %>%
-    mutate(record_id = jhh_007_2022_record_id)
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   baseline2021 %>%
+#     filter(record_id == jhh_007_2021_record_id) %>%
+#     process_baseline() %>%
+#     mutate(record_id = jhh_007_2022_record_id)
+# )
 
-redcap_upload(
-  2022,
-  "baseline_arm_1",
-  baseline2021 %>%
-    filter(record_id == pch_043_2021_record_id) %>%
-    process_baseline() %>%
-    mutate(record_id = pch_043_2022_record_id)
-)
+# redcap_upload(
+#   2022,
+#   "baseline_arm_1",
+#   baseline2021 %>%
+#     filter(record_id == pch_043_2021_record_id) %>%
+#     process_baseline() %>%
+#     mutate(record_id = pch_043_2022_record_id)
+# )
 
 #
 # SECTION Covid vaccines
@@ -332,33 +343,34 @@ redcap_covax_request <- function(project_year) {
 }
 
 covax2021 <- redcap_covax_request(2021)
+covax2022 <- redcap_covax_request(2022)
 
-covax2022 <- covax2021 %>%
+covax2022_active <- covax2022 %>%
   select(-contains("redcap")) %>%
-  filter(record_id %in% active2021$record_id)
+  filter(record_id %in% active2022$record_id)
 
-redcap_upload_spoonfed(10, 2022, "vaccination_arm_1", covax2022)
+# redcap_upload_spoonfed(10, 2023, "vaccination_arm_1", covax2022_active)
 
-redcap_upload(
-  2022,
-  "vaccination_arm_1",
-  covax2021 %>%
-    select(-contains("redcap")) %>%
-    filter(record_id == "170-71")
-)
+# redcap_upload(
+#   2022,
+#   "vaccination_arm_1",
+#   covax2021 %>%
+#     select(-contains("redcap")) %>%
+#     filter(record_id == "170-71")
+# )
 
-redcap_upload(
-  2022,
-  "vaccination_arm_1",
-  covax2021 %>%
-    select(-contains("redcap")) %>%
-    filter(record_id == "170-31")
-)
+# redcap_upload(
+#   2022,
+#   "vaccination_arm_1",
+#   covax2021 %>%
+#     select(-contains("redcap")) %>%
+#     filter(record_id == "170-31")
+# )
 
-redcap_upload(
-  2022,
-  "vaccination_arm_1",
-  covax2021 %>%
-    select(-contains("redcap")) %>%
-    filter(record_id == "168-4")
-)
+# redcap_upload(
+#   2022,
+#   "vaccination_arm_1",
+#   covax2021 %>%
+#     select(-contains("redcap")) %>%
+#     filter(record_id == "168-4")
+# )
