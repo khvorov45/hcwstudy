d34ea6abe587359260f1cc7778275943289e12c9
Arseniy Khvorov <khvorov45@gmail.com>
2023-02-14 12:35:54 +1100

    more counts

--- report/table-bleed-counts/table-bleed-counts.R
+++ report/table-bleed-counts/table-bleed-counts.R
@@ -21,7 +21,7 @@ serology %>%
     filter(pid == "QCH-071", year == 2020, day %in% c(0, 14)) %>%
     pivot_wider(names_from = "day", values_from = "titre")
 
-serology %>% 
+serology_for_counting <- serology %>% 
     left_join(prior_vac_counts, "pid") %>%
     mutate(prior_study_year = case_when(
         year == 2020 ~ prior2020,
@@ -32,30 +32,48 @@ serology %>%
     )) %>%
     select(pid, year, day, prior_study_year, vax_inf) %>%
     distinct() %>%
+    mutate(across(c(year, prior_study_year, day), as.character))
+
+#bind_rows(summarise(group_by(serology_for_counting, vax_inf, year, prior_study_year), n = length(unique(pid)), day = "Subjects", .groups = "drop")) %>%
+    #bind_rows(summarise(group_by(serology_for_counting, vax_inf, day, prior_study_year), n = length(unique(pid)), year = "Subjects", .groups = "drop")) %>%
+
+serology_for_counting %>%
     count(vax_inf, day, year, prior_study_year) %>%
-    mutate(across(c(year, prior_study_year, day), as.character)) %>%
     bind_rows(summarise(group_by(., vax_inf, day, year), n = sum(n), prior_study_year = "Total", .groups = "drop")) %>%
     bind_rows(summarise(group_by(., vax_inf, day, prior_study_year), n = sum(n), year = "Total", .groups = "drop")) %>%
     bind_rows(summarise(group_by(., vax_inf, year, prior_study_year), n = sum(n), day = "Total", .groups = "drop")) %>%
+    bind_rows(summarise(group_by(serology_for_counting, vax_inf, day, prior_study_year), n = length(unique(pid)), year = "Subjects", .groups = "drop")) %>%
+    bind_rows(summarise(group_by(serology_for_counting, vax_inf, day), n = length(unique(pid)),  prior_study_year = "Total", year = "Subjects", .groups = "drop")) %>%
+    bind_rows(summarise(group_by(serology_for_counting, vax_inf, year, prior_study_year), n = length(unique(pid)), day = "Subjects", .groups = "drop")) %>%
+    bind_rows(summarise(group_by(serology_for_counting, vax_inf, year), n = length(unique(pid)),  prior_study_year = "Total", day = "Subjects", .groups = "drop")) %>%
+    bind_rows(summarise(group_by(serology_for_counting, vax_inf, prior_study_year), n = length(unique(pid)), day = "Subjects", year = "Subjects", .groups = "drop")) %>%
+    bind_rows(summarise(group_by(serology_for_counting, vax_inf), n = length(unique(pid)),  prior_study_year = "Total", day = "Subjects", year = "Subjects", .groups = "drop")) %>%
     mutate(
-        day = fct_relevel(day, "0", "7", "14", "30"), 
+        year = fct_relevel(year, "2020", "2021", "2022", "Total", "Subjects"),
+        day = fct_relevel(day, "0", "7", "14", "30", "220", "Total", "Subjects"), 
         vax_inf = fct_relevel(vax_inf, "V") %>% recode("V" = "Vaccination", "I" = "Infection")
     ) %>%
     arrange(vax_inf, day, prior_study_year) %>%
     rename(Bleed = vax_inf, Day = day, Year = year) %>%
     pivot_wider(names_from = "prior_study_year", values_from = "n", values_fill = 0) %>%
-    kbl(
-        format = "latex",
-        caption = "Counts of bleeds with measured antibody titres for each timepoint (day post-vaccination) 
-        over the study years for different prior vaccination groups.",
-        booktabs = TRUE,
-        label = "routine-bleed-counts"
-    ) %>%
-    add_header_above(c(" " = 3, "Known vaccinations in the 5 years before bleed" = 6)) %>%
-    column_spec(4:9, width = "1cm") %>%
-    collapse_rows(columns = 1, latex_hline = "major") %>%
-    collapse_rows(columns = 2, latex_hline = "linespace") %>%
-    write("report/table-bleed-counts/routine-bleed-counts.tex")
+    group_by(Bleed) %>%
+    group_walk(function(subset, key) {
+        subset %>%
+            kbl(
+                format = "latex",
+                caption = glue::glue("Counts of {tolower(key$Bleed)} bleeds with measured antibody titres for each timepoint (day post-{tolower(key$Bleed)})
+                over the study years for different prior vaccination groups.
+                Rows labelled 'Total' sum all of the corresponding bleeds. 
+                Rows labelled 'Subjects' count unique subjects in the corresponding categories."),
+                booktabs = TRUE,
+                label = glue::glue("bleed-counts-{key$Bleed}")
+            ) %>%
+            add_header_above(c(" " = 2, "Known vaccinations in the 5 years before bleed" = 6)) %>%
+            column_spec(4:9, width = "1cm") %>%
+            collapse_rows(columns = 1, latex_hline = "major") %>%
+            # collapse_rows(columns = 2, latex_hline = "linespace") %>%
+            write(glue::glue("report/table-bleed-counts/bleed-counts-{key$Bleed}.tex"))
+    })
 
 covid_serology <- read_csv("data/serology-covid.csv", col_types = cols())
 
