329c877ccff15bc494eaa36a427bf1ca5d44a270
Arseniy Khvorov <khvorov45@gmail.com>
2023-02-17 15:40:42 +1100

    add vaccination patterns

--- report/missing-vachist/missing-vachist.R
+++ report/missing-vachist/missing-vachist.R
@@ -30,7 +30,7 @@ known_prior_vac_counts %>%
     pivot_longer(starts_with("atleast"), names_to = "known_prior", values_to = "status") %>%
     mutate(
         known_prior = recode(known_prior, "atleast0" = "Total", "atleast5" = "All 5") %>%
-            str_replace("atleast", "At least ") %>% 
+            str_replace("atleast", "At least ") %>%
             factor(c("All 5", "At least 4", "At least 3", "At least 2", "At least 1", "Total")),
         reference_year = str_replace(reference_year, "known_prior", ""),
         recruitment_year = as.character(recruitment_year),
@@ -44,7 +44,7 @@ known_prior_vac_counts %>%
     (function(x) {write_csv(x, "report/missing-vachist/missing-vachist.csv"); x}) %>%
     kbl(
         format = "latex",
-        caption = "Counts of participants split by the number of years for which we have their vaccination history. 
+        caption = "Counts of participants split by the number of years for which we have their vaccination history.
             \"Ref year\" is the reference year the 5 prior years' vaccination history relates to.
             For example, if the reference year is 2022 then the 5 years prior to that are 2021, 2020, 2019, 2018 and 2017.
             If we know someone's vaccination status in 2021, 2020, 2019 but not in 2018 and 2017 then they would have 3 \"prior years with known status\".
@@ -57,3 +57,29 @@ known_prior_vac_counts %>%
     column_spec(2, width = "2cm") %>%
     collapse_rows(columns = 1, latex_hline = "major") %>%
     write("report/missing-vachist/missing-vachist.tex")
+
+vac_hist_all_combos <- vac_hist %>%
+    select(pid, year, status) %>%
+    arrange(year) %>%
+    pivot_wider(names_from = "year", values_from = "status") %>%
+    count(pick(starts_with("2"))) %>%
+    rename(Count = n) %>%
+    mutate(across(starts_with("2"), ~replace_na(.x, "Not recorded"))) %>%
+    arrange(desc(Count))
+
+vac_hist_all_combos %>%
+    filter(Count >= 10) %>%
+    write_csv("report/missing-vachist/vachist-combos.csv") %>%
+    kbl(
+        format = "latex",
+        caption = glue::glue(
+            "Counts of the most common (represented by at least 10 subjects) vaccination history combinations.
+            The table accounts for {vac_hist_all_combos %>% filter(Count >= 10) %>% pull(Count) %>% sum()} out of
+            the {vac_hist_all_combos %>% pull(Count) %>% sum()} total subjects."
+        ),
+        booktabs = TRUE,
+        label = "vachist-combos"
+    ) %>%
+    kable_styling(latex_options = "scale_down") %>%
+    str_replace_all("Not recorded", "\\\\textcolor{gray}{(missing)}") %>%
+    write("report/missing-vachist/vachist-combos.tex")
