4193c97287e424dfcb1a227d4c2d8054c8d12dd4
Arseniy Khvorov <khvorov45@gmail.com>
2023-02-23 08:50:58 +1100

    put copies into pull

--- data-raw/pull-NIHHCWserol.sh
+++ data-raw/pull-NIHHCWserol.sh
@@ -6,3 +6,15 @@ cp "/home/khvorova/vidrlwhoflu/Research/2019.176 NIH HCW Study/Serology/NIHHCW_C
 
 cp "/home/khvorova/vidrlwhoflu/Group/AF_LC_RT_share/Annie Z PhD/Chapter 1/NIHHCW FLU Landscapes.accdb" \
     "/home/khvorova/Projects/hcwstudy/data-raw/serology-landscapes/NIHHCW FLU Landscapes.accdb"
+
+cp "/home/khvorova/vidrlwhoflu/Group/AF_LC_RT_share/NIH Aust HCW Study/2022 Y3 Serology/InDevr Data/V72 H1 egg/Y3_2023_H1_egg_all.xlsx" \
+    "data-raw/serology/Y3_2023_H1_egg_all.xlsx"
+
+cp "/home/khvorova/vidrlwhoflu/Group/AF_LC_RT_share/NIH Aust HCW Study/2022 Y3 Serology/InDevr Data/V76 BVic Egg/BVic_egg_2022_all.xlsx" \
+    "data-raw/serology/BVic_egg_2022_all.xlsx"
+
+cp "/home/khvorova/vidrlwhoflu/Research/2019.176 NIH HCW Study/Serology/New Caledonia HI_H1N1 immunogenicity_allplates.xlsx" \
+    "data-raw/serology/New Caledonia HI_H1N1 immunogenicity_allplates.xlsx"
+
+cp "/home/khvorova/vidrlwhoflu/Group/AF_LC_RT_share/NIH Aust HCW Study/H1N1 immunogenicity study/Serology against alternate Vaccine Year H1 strains_and Br78_matched_2020_2021_sera/H1N1 immunogenicity study_Br78_serology/NIH_H1N1_ABrazil_11_egg_All.xlsx" \
+    "data-raw/serology/NIH_H1N1_ABrazil_11_egg_All.xlsx"
