acb1ff9ed139b2090b3830c4187106274a1402dd
Arseniy Khvorov <khvorov45@gmail.com>
2023-02-23 08:50:40 +1100

    remove pull from export

--- data-raw/export-NIHHCWserol.sh
+++ data-raw/export-NIHHCWserol.sh
@@ -21,9 +21,3 @@ mdb-schema "data-raw/serology-landscapes/NIHHCW FLU Landscapes.accdb" |
 grep -oP "CREATE TABLE \[\K.+(?=\])" |
 # Export each table into its own file under NIGGCWserol folder
 while read line ; do mdb-export "data-raw/serology-landscapes/NIHHCW FLU Landscapes.accdb" "$line" > "data-raw/serology-landscapes/$line.csv" ; done
-
-cp "/home/khvorova/vidrlwhoflu/Group/AF_LC_RT_share/NIH Aust HCW Study/2022 Y3 Serology/InDevr Data/V72 H1 egg/Y3_2023_H1_egg_all.xlsx" \
-    "data-raw/serology/Y3_2023_H1_egg_all.xlsx"
-
-cp "/home/khvorova/vidrlwhoflu/Group/AF_LC_RT_share/NIH Aust HCW Study/2022 Y3 Serology/InDevr Data/V76 BVic Egg/BVic_egg_2022_all.xlsx" \
-    "data-raw/serology/BVic_egg_2022_all.xlsx"
