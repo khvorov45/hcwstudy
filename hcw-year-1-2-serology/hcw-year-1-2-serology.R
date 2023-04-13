a14b59ec8e4d1f95e2c6d977902d4579e4c16772
Arseniy Khvorov <khvorov45@gmail.com>
2023-04-13 12:44:49 +1000

    add a plot

--- hcw-year-1-2-serology/hcw-year-1-2-serology.R
+++ hcw-year-1-2-serology/hcw-year-1-2-serology.R
@@ -532,7 +532,7 @@ contributing_participants_wider <- contributing_participants %>%
 addifnotempty <- function(varname1, varname2, subset) {
     n <- nrow(subset)
     if (n > 0) {
-        glue::glue("{varname1} -> {varname2} [label = 'n={n}']")
+        glue::glue("{varname1} -> {varname2} [label = 'n={n}', penwidth = {n / 1000 * 3 + 0.1}]")
     } else {
         ""
     }
@@ -596,3 +596,103 @@ strobe_raw <- strobe %>% DiagrammeRsvg::export_svg() %>% charToRaw()
 
 rsvg::rsvg_pdf(strobe_raw, "hcw-year-1-2-serology/strobe.pdf")
 rsvg::rsvg_png(strobe_raw, "hcw-year-1-2-serology/strobe.png")
+
+all_dates <- bleed_dates %>%
+    filter(day %in% c(0, 14, 220)) %>%
+    mutate(day = as.character(day)) %>%
+    bind_rows(vaccinations %>% filter(!is.na(vaccination_date)) %>% select(pid, year, date = vaccination_date) %>% mutate(day = "vax")) %>%
+    filter(year %in% c(2020, 2021)) %>%
+    mutate(day = factor(day, levels = c("0", "vax", "14", "220"), labels = c("Pre-vaccination", "Vaccination", "Post-vaccination", "End-of-year"))) %>%
+    arrange(pid, year, day) %>%
+    # NOTE(sen) Filter dates that don't make sense (likely data entry errors)
+    filter(.by = c(pid, year), date - lag(date, default = ymd("2000-01-01")) > -3)
+
+bleed_intervals <- all_dates %>%
+    pivot_wider(names_from = "day", values_from = "date") %>%
+    mutate(
+        i0_vax = `Vaccination` - `Pre-vaccination`,
+        ivax_14 = `Post-vaccination` - `Vaccination`,
+        ivax_220 = `End-of-year` - `Vaccination`,
+    )
+
+bleed_dates_plots <- all_dates %>%
+    left_join(bleed_intervals, c("pid", "year")) %>%
+    group_by(year) %>%
+    group_map(.keep = TRUE, function(data, key) {
+        this_year <- key$year
+        date_min <- lubridate::ymd(glue::glue("{this_year}-03-01"))
+        date_max <- lubridate::ymd(glue::glue("{this_year + 1}-01-01"))
+        pl <- data %>%
+            mutate(
+                pid = fct_reorder(fct_drop(pid), replace_na(as.integer(`Pre-vaccination`), 0), .desc = TRUE),
+            ) %>%
+            ggplot(aes(date, pid, color = day, shape = day)) +
+            theme_bw() +
+            theme(
+                axis.text.y = element_blank(),
+                axis.ticks.y = element_blank(),
+                axis.title.y = element_blank(),
+                axis.title.x = element_blank(),
+                panel.grid.major.y = element_blank(),
+                panel.grid.minor.y = element_blank(),
+                strip.background = element_blank(),
+                panel.spacing = unit(0, "null"),
+            ) +
+            coord_cartesian(xlim = c(date_min, date_max)) +
+            facet_wrap(~year, strip.position = "right") +
+            scale_y_discrete(expand = expansion(0.05, 0)) +
+            scale_x_date("", breaks = lubridate::ymd(paste0(this_year, "-", 1:12, "-", 1)), labels = function(breaks) {
+                month.abb[lubridate::month(breaks)]
+            }) +
+            scale_color_manual("", values = viridis::viridis_pal(option = "C", end = 0.8)(4)) +
+            scale_shape_manual("", values = c(19, 3, 15, 17)) +
+            geom_point() +
+            geom_point(data = . %>% filter(day == "vax")) +
+            geom_text(
+                aes(x = date_min, y = 10, label = glue::glue("Participants bled: {pids}")),
+                hjust = 0,
+                inherit.aes = FALSE, data = summarise(contributing_participants, .by = year, pids = length(unique(pid))) %>% filter(year == this_year)
+            )
+        if (this_year != max(all_dates$year)) {
+            pl <- pl + theme(
+                axis.text.x = element_blank(),
+                axis.ticks.x = element_blank(),
+            )
+        }
+        pl
+    })
+
+bleed_dates_plot <- ggpubr::ggarrange(plotlist = bleed_dates_plots, common.legend = TRUE, ncol = 1)
+
+(function(name, ...) {ggsave(paste0(name, ".pdf"), ...);ggsave(paste0(name, ".png"), ...)})(
+    "hcw-year-1-2-serology/bleed_dates", bleed_dates_plot, width = 15, height = 20, units = "cm"
+)
+
+bleed_intervals_plot <- bleed_intervals %>%
+    select(pid, year, starts_with("i")) %>%
+    pivot_longer(starts_with("i"), names_to = "timepoint", values_to = "interval") %>%
+    filter(!is.na(interval)) %>%
+    mutate(
+        interval = as.integer(interval),
+        timepoint = factor(timepoint, c("i0_vax", "ivax_7", "ivax_14", "ivax_220", "i0_flupos", "ivax_flupos"))
+    ) %>%
+    ggplot(aes(timepoint, interval)) +
+    theme_bw() +
+    theme(
+        panel.grid.minor = element_blank(),
+        strip.background = element_blank(),
+        panel.spacing = unit(0, "null"),
+        axis.text.x = element_text(angle = 30, hjust = 1),
+        axis.title.x = element_blank(),
+        plot.margin = margin(1, 1, 1, 50),
+    ) +
+    facet_wrap(~year) +
+    scale_y_continuous("Days", breaks = c(0, 14, 30, 60, 90, 120, 150, 180, 220)) +
+    scale_x_discrete("Interval", labels = c("Prevaccination to vaccination", "Vaccination to post-vaccination", "Vaccination to end-of-year")) +
+    geom_jitter(shape = 18, width = 0.2, alpha = 0.3, color = "gray50") +
+    geom_boxplot(color = "blue", fill = NA, outlier.alpha = 0)
+
+(function(name, ...) {ggsave(paste0(name, ".pdf"), ...);ggsave(paste0(name, ".png"), ...)})(
+    "hcw-year-1-2-serology/bleed_intervals", bleed_intervals_plot, width = 15, height = 12, units = "cm"
+)
+
